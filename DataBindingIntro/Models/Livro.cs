1237ca52a3efcd11ab9641c8a372345732240201
Yan Paulo <yanpaulo@hotmail.com>
2017-07-10 20:30:51 -0300

    Adicionar arquivos de projeto.

---
+++ DataBindingIntro/Models/Livro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindingIntro.Models
+{
+    public class Livro
+    {
+        public int Codigo { get; set; } = 999;
+
+        public string Nome { get; set; } = "Sem nome";
+
+
+
+        public override string ToString() 
+            => $"Cod.: {Codigo}, Livro: {Nome}";
+    }
+}
